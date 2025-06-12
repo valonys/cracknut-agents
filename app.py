9774fd2e1b47433ad5dfb63cffe2093a7155bfc3
Ataliba Miguel <ataliba.miguel@valonylabs.com>
2025-06-12 10:38:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -52,9 +52,17 @@ with st.sidebar:
         index=0,
     )
     st.header("📁 Document Hub")
-    uploaded_files = st.file_uploader("Upload up to 10 PDFs", type=["pdf"], accept_multiple_files=True)
+    uploaded_files: # = st.file_uploader("Upload up to 10 PDFs", type=["pdf"], accept_multiple_files=True)
     if uploaded_files:
+        #st.session_state.faiss_db = None
+    if "faiss_initialized" not in st.session_state:
+        st.session_state.faiss_db = process_uploaded_pdfs(uploaded_files)
+        st.session_state.faiss_initialized = True
+        st.sidebar.success(f"✅ {len(uploaded_files)} PDF(s) processed and indexed.")
+   else:
         st.session_state.faiss_db = None
+        st.session_state.faiss_initialized = False
+
 
 # --- FAISS Setup ---
 @st.cache_resource
