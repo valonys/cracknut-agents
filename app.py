cd78c025b15a56ad67b244e9e2abaee6aea3744a
Ataliba Miguel <ataliba.miguel@valonylabs.com>
2025-06-12 11:22:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import tempfile
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 # --- ENV ---
